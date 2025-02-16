b69cb493071d1dc67fcea093803271b08891310d
k1flar <den1s-davydov@mail.ru>
2025-02-16 12:34:34 +0300

    При развертывании окружения применяются миграции

--- service/cmd/cli/main.go
+++ service/cmd/cli/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"log/slog"
 	"os"
 
+	"github.com/jmoiron/sqlx"
 	dotenv "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	err := dotenv.Load()
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
+		os.Exit(1)
 	}
 
-	fmt.Println("this is a cli")
-	fmt.Println("args: ", os.Args)
+	db, err := sqlx.Connect("postgres", os.Getenv("PG_DSN"))
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
 }
--- service/go.mod
+++ service/go.mod
@@ -2,4 +2,9 @@ module github.com/K1flar/crawlers
 
 go 1.23.6
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
