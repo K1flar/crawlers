c39f85960021b0df86d09059f455813dbd835470
k1flar <den1s-davydov@mail.ru>
2025-02-24 01:36:36 +0300

    Изменил типы PK в миграциях

--- service/db/migrations/20250215221342_create_sources_table.up.sql
+++ service/db/migrations/20250215221342_create_sources_table.up.sql
@@ -1,8 +1,12 @@
 CREATE TABLE IF NOT EXISTS sources (
-    id SERIAL PRIMARY KEY,
-    task_id INT REFERENCES tasks(id) ON DELETE CASCADE,
+    id BIGSERIAL PRIMARY KEY,
+    task_id BIGINT REFERENCES tasks(id) ON DELETE CASCADE,
     title TEXT NOT NULL,
     url TEXT NOT NULL,
     status TEXT NOT NULL DEFAULT 'available',
     weight FLOAT NOT NULL
+    uuid uuid NOT NULL
+    parent_uuid uuid
+
+    UNIQUE (uuid)
 )
