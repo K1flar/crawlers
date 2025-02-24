4b6b97fc75c9e88bd861ab84c222d59f03050b9f
k1flar <den1s-davydov@mail.ru>
2025-02-24 13:50:07 +0300

    Перенес crawler'а в слой services

--- service/internal/services/interfaces.go
+++ service/internal/services/interfaces.go
@@ -1,6 +1,15 @@
 package services
 
-import "github.com/K1flar/crawlers/internal/models/page"
+import (
+	"context"
+
+	"github.com/K1flar/crawlers/internal/models/page"
+	"github.com/K1flar/crawlers/internal/models/task"
+)
+
+type Crawler interface {
+	Start(ctx context.Context, task task.Task) error
+}
 
 type CollectionCollector interface {
 	AddPage(uuid string, page page.Page)
